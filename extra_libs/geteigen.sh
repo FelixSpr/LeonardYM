0a23e97392dfdc5c4446d32d50a4b0068071d7b6
David Schaich <daschaich@gmail.com>
2021-02-02 19:26:07 +0000

    Update source location

--- extra_libs/geteigen.sh
+++ extra_libs/geteigen.sh
@@ -1,3 +1,8 @@
-wget http://bitbucket.org/eigen/eigen/get/3.3.7.tar.bz2
-tar -xvjf 3.3.7.tar.bz2
-mv eigen-* Eigen
+v=3.3.9
+
+if [ ! -f eigen-$v.tar.bz2 ] ; then
+  wget https://gitlab.com/libeigen/eigen/-/archive/$v/eigen-$v.tar.bz2
+fi
+rm -f Eigen
+tar -xvjf eigen-$v.tar.bz2
+ln -s eigen-$v Eigen
